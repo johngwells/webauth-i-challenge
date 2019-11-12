79bd3d1f2c281bb04bad8d04e05f2dbd2298638b
John Wells <molekewl2017@gmail.com>
2019-11-12 12:47:20 -0800

    setting up /register & /login

---
+++ auth/auth-router.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcrypt');
+
+const router = require('express').Router();
+
+// Create Users & models 
+
+// /register post request
+
+// /login post request
+
+module.exports = router;
