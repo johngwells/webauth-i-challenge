a98ed645f689ddd57d3bcb2ea243473dbbd37ef0
John Wells <molekewl2017@gmail.com>
2019-11-12 13:28:31 -0800

    login & register logic

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,10 +2,31 @@ const bcrypt = require('bcrypt');
 
 const router = require('express').Router();
 
-// Create Users & models 
+const Users = require('../users/users.model');
 
-// /register post request
+router.post('/register', (req, res) => {
+  let user = req.body;
 
-// /login post request
+  const hash = bcrypt.hashSync(user.password, 14);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => res.status(201).json(saved))
+    .catch(error => res.status(500).json(error));
+})
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+  .first()
+  .then(user => {
+    user
+      ? res.status(200).json({ message: `Welcome ${user.username}` })
+      : res.status(401).json({ error: 'Invalid Credentials' })
+  })
+  .catch(error => res.status(500).json(error));
+})
 
 module.exports = router;
