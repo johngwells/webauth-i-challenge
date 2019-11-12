106ddf1ecafadbe20740ec8024281b65d8ae73d5
John Wells <molekewl2017@gmail.com>
2019-11-12 12:42:45 -0800

    add route to auth-router.js

--- api/api-router.js
+++ api/api-router.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
 // Routes Here
+const authRouter = require('../auth/auth-router');
+
+router.use('/auth', authRouter);
 
 router.get('/', (req, res) => {
   res.json({ api: 'Its alive' });
