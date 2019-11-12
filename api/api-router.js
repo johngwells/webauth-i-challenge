50355a97930df38c3b10c5ebc9e8b9bad42116d7
John Wells <molekewl2017@gmail.com>
2019-11-12 13:18:49 -0800

    connect routes to /users

--- api/api-router.js
+++ api/api-router.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 // Routes Here
 const authRouter = require('../auth/auth-router');
+const usersRouter = require('../users/users-router');
 
 router.use('/auth', authRouter);
+router.use('/users', usersRouter);
 
 router.get('/', (req, res) => {
   res.json({ api: 'Its alive' });
