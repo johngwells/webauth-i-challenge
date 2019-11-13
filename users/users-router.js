7d1067027594429855292722138bd67c2d944e3a
John Wells <molekewl2017@gmail.com>
2019-11-13 12:41:58 -0800

    logout feature

--- users/users-router.js
+++ users/users-router.js
@@ -3,8 +3,9 @@ const router = require('express').Router();
 const Users = require('./users-model');
 
 const requiresAuth = require('../middleware/require-auth');
+const restricted = require('../middleware/restricted');
 
-router.get('/', (req, res) => {
+router.get('/', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users)
