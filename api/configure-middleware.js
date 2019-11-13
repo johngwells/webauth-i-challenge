a950e1f6b69eb1ef51dbf175fe10556c3160f573
John Wells <molekewl2017@gmail.com>
2019-11-13 12:41:01 -0800

    add express session logic for cookies

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 
 module.exports = server => {
+  const sessionConfig = {
+    name: 'name',
+    secret: 'keep it secret',
+    cookie: {
+      maxAge: 1000 * 30,
+      secure: false, // true in production
+      httpOnly: true
+    },
+    resave: false,
+    saveOnInitialized: false // GDPR laws setting cookies auto
+  };
+  
   server.use(helmet());
   server.use(express.json());
   server.use(cors());
+  server.use(session(sessionConfig))
 };
